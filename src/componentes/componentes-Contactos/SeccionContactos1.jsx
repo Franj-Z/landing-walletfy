30941a6329a51422f6c5e2c43efc89e6815b0a3f
cristian-bosz <cristianbosz@gmail.com>
2022-04-14 12:16:29 -0300

    cambie la descripcion del formulario

--- src/componentes/componentes-Contactos/SeccionContactos1.jsx
+++ src/componentes/componentes-Contactos/SeccionContactos1.jsx
@@ -7,7 +7,7 @@ const SeccionContactos1 = () => {
                         <div className='row justify-content-center'>
                             <h2 className='text-center display-5 my-4'>Contactos</h2>
                             <div className="col-md-8 borde pb-3 ">
-                                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dicta maiores officiis laudantium, libero tempore exercitationem, ratione, non modi sapiente cupiditate et! Quo, ab deserunt voluptatum sit delectus corporis. Amet?</p>
+                                <p className="lead">Si tienes alguna consulta no dudes en contactarnos.</p>
                             </div>
                         </div>   
             </section>
